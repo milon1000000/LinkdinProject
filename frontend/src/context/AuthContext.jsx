6233d202dd5412aff39cef3f8b314850fd52e408
milon1000000 <milonmondolmd33@gmail.com>
2026-04-28 13:43:32 +0600

    Update server URL in AuthContext

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,7 +1,7 @@
 import React, { createContext } from 'react'
  export const authDataContext=createContext();
 function AuthContext({children}) {
-    const serverUrl="https://linkdinproject.onrender.com"
+    const serverUrl="https://linkdinproject-u4ar.onrender.com"
     const value={
         serverUrl
     }
